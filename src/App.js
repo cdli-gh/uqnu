5f2812835d60b1f6cb94b3f4c47c5c9f0f308839
Khoidt <ekh.itd@gmail.com>
2021-08-06 11:25:04 +0200

    fix App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.css";
 //import logo from './logo.svg';
-import { ATFUploader } from './features/loader/uploader';
-//import { fetchATF } from "./services/API";
-import { Editor } from './features/editor/Editor';
 
-/* function App () {
+import { ATFUploader } from './features/loader/uploader';
+import { Editor } from './features/editor/Editor';
+import { fetchATF } from "./services/API";
+
+function App () {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
     async function getData() {
@@ -27,76 +28,12 @@ import { Editor } from './features/editor/Editor';
     // will fetch the ATF data from given ID's in URL
     getData();
   }, []);
-}; */
-
-function App() {
-    
-    return (
+  
+  return (
     <div className="App">
-    <ATFUploader/>
+      <ATFUploader />
     </div>
-    )
+  );
 };
 
 export default App;
-
-/* function App() {
-    
-    return (
-    <div className="App">
-    <ATFUploader/>
-    </div>
-    )
-};
-    
-   //older:
-   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Editor />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-} */
